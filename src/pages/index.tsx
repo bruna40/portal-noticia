5c64557aaa956bdcb74bf04e1f4a2e8edac597bf
Bruna Santiago <brunasanthiago@outlook.com>
2023-06-13 09:36:34 -0300

    Logica: Reafatorando a pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,33 +1,26 @@
 import Link from "next/link";
 import { ContainerHome } from "../styles/pages";
-import { useEffect, useState } from "react";
+import { GetServerSideProps } from "next";
+import Image from "next/image";
+
+interface News {
+  title: string;
+  image: string;
+  description: string;
+}
 
 
-export default function Home() {
-  const [news, setNews] = useState<any>([]) 
-  const fetchAllData = async () => {
-    try {
-      const URL = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=dc0bca51b354335a2666ae0abaa8d50c';
-      const response = await fetch(URL);
-      const data = await response.json();
-      setNews(data.articles)
-    } catch (error) {
-      console.log(error)
-    }
-  }
+export default function Home({ news }) {
 
-  useEffect(() => {
-    fetchAllData()
-  }, [])
   return (
     <ContainerHome>
       {
-        news.map((item: any) => {
+        news.map((item: News) => {
           return (
             <div key={item.title}>
               <h3>{item.title}</h3>
               <span>
-                <img src={item.image} alt="imagem" />
+                <img src={item.image} alt="imagem" width={4} height={4}/>
                 <p>{item.description}</p>
                 <Link href={`/new/${item.title}`}>
                   Ver notícia completa
@@ -40,4 +33,17 @@ export default function Home() {
       }
     </ContainerHome>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const URL = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=dc0bca51b354335a2666ae0abaa8d50c';
+  const response = await fetch(URL);
+  const data = await response.json();
+  const news = data.articles
+
+  return {
+    props: {
+      news
+    }
+  }
 }
--- src/pages/new/[id].tsx
+++ src/pages/new/[id].tsx
@@ -1,15 +1,40 @@
+import { GetServerSideProps } from "next/types";
 import { ContainerNew } from "./style";
 
-export default function New() {
+export default function New({ news }: any) {
 
   return (
     <ContainerNew>
-        <h1>Titulo reportagem: exemploxxxx</h1>
-        <p>Autor: Bruna</p>
-        <article>
-            <p>Conteudo da reportagem</p>
-        </article>
+        {
+            news.map((item: any) => {
+                return (
+                    <>
+                        <h1>{item.title}</h1>
+                        <p>Autor: {item.source.name}</p>
+                        <article>
+                            {item.content}
+                        </article>
+                    </>
+                )
+            }
+            )
+        }
     </ContainerNew>
 
   )
+}
+
+// como fazer o fetch de uma noticia especifica, que vem da pagina index.tsx, mas trazer pelo titulo
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { title } = context.query;
+    const URL = `https://gnews.io/api/v4/search?q=${title}&lang=en&country=us&max=10&apikey=dc0bca51b354335a2666ae0abaa8d50c`;
+    const response = await fetch(URL);
+    const data = await response.json();
+    const news = data.articles
+    return {
+        props: {
+            news
+        }
+    }
 }
