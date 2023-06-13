38d77f7af3245078d4f2689575906f9dcb08fb58
Bruna Santiago <brunasanthiago@outlook.com>
2023-06-13 08:12:24 -0300

    Logica: mostrar as noticias na pagina home feito

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,72 +1,43 @@
 import Link from "next/link";
 import { ContainerHome } from "../styles/pages";
-
+import { useEffect, useState } from "react";
 
 
 export default function Home() {
+  const [news, setNews] = useState<any>([]) 
+  const fetchAllData = async () => {
+    try {
+      const URL = 'https://gnews.io/api/v4/search?q=example&lang=en&country=us&max=10&apikey=dc0bca51b354335a2666ae0abaa8d50c';
+      const response = await fetch(URL);
+      const data = await response.json();
+      setNews(data.articles)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchAllData()
+  }, [])
   return (
     <ContainerHome>
-      <div>
-        <h3>Google Brings AI to Priceline to Help Plan Your Vacations</h3>
-        <span>
-          <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/fc2820734f15653be47defc6c91b2770.jpg" alt="imagem" />
-          <p>With access to Google’s generative AI, Priceline will be able to extract data on hotels, for example, more efficiently.</p>
-          <Link href="/new/1">
-            Ver notícia completa
-          </Link>
-        </span>
-      </div>
-      <div>
-        <h3>Google Brings AI to Priceline to Help Plan Your Vacations</h3>
-        <span>
-          <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/fc2820734f15653be47defc6c91b2770.jpg" alt="imagem" />
-          <p>With access to Google’s generative AI, Priceline will be able to extract data on hotels, for example, more efficiently.</p>
-          <Link href="/new/1">
-            Ver notícia completa
-          </Link>
-        </span>
-      </div>
-      <div>
-        <h3>Google Brings AI to Priceline to Help Plan Your Vacations</h3>
-        <span>
-          <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/fc2820734f15653be47defc6c91b2770.jpg" alt="imagem" />
-          <p>With access to Google’s generative AI, Priceline will be able to extract data on hotels, for example, more efficiently.</p>
-          <Link href="/new/1">
-            Ver notícia completa
-          </Link>
-        </span>
-      </div>
-      <div>
-        <h3>Google Brings AI to Priceline to Help Plan Your Vacations</h3>
-        <span>
-          <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/fc2820734f15653be47defc6c91b2770.jpg" alt="imagem" />
-          <p>With access to Google’s generative AI, Priceline will be able to extract data on hotels, for example, more efficiently.</p>
-          <Link href="/new/1">
-            Ver notícia completa
-          </Link>
-        </span>
-      </div>
-      <div>
-        <h3>Google Brings AI to Priceline to Help Plan Your Vacations</h3>
-        <span>
-          <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/fc2820734f15653be47defc6c91b2770.jpg" alt="imagem" />
-          <p>With access to Google’s generative AI, Priceline will be able to extract data on hotels, for example, more efficiently.</p>
-          <Link href="/new/1">
-            Ver notícia completa
-          </Link>
-        </span>
-      </div>
-      <div>
-        <h3>Google Brings AI to Priceline to Help Plan Your Vacations</h3>
-        <span>
-          <img src="https://i.kinja-img.com/gawker-media/image/upload/c_fill,f_auto,fl_progressive,g_center,h_675,pg_1,q_80,w_1200/fc2820734f15653be47defc6c91b2770.jpg" alt="imagem" />
-          <p>With access to Google’s generative AI, Priceline will be able to extract data on hotels, for example, more efficiently.</p>
-          <Link href="/new/1">
-            Ver notícia completa
-          </Link>
-        </span>
-      </div>
-
+      {
+        news.map((item: any) => {
+          return (
+            <div key={item.title}>
+              <h3>{item.title}</h3>
+              <span>
+                <img src={item.image} alt="imagem" />
+                <p>{item.description}</p>
+                <Link href={`/new/${item.title}`}>
+                  Ver notícia completa
+                </Link>
+              </span>
+            </div>
+          )
+        }
+        )
+      }
     </ContainerHome>
   )
 }
